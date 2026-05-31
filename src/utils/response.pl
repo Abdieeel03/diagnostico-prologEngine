af01a784768026f60c2a61ae7c46c1dc3ee30c6f
Abdieeel03 <marcelopostigo54@gmail.com>
2026-05-31 13:49:07 -0500

    Se agregó el módulo de respuesta con funciones para manejar respuestas exitosas y de error en formato JSON.

---
+++ src/utils/response.pl
@@ -0,0 +1,36 @@
+:- module(response, [
+  success_response/2,
+  success_response/3,
+  error_response/2,
+  error_response/3
+]).
+
+:- use_module(library(http/http_json)).
+
+success_response(Message, Data) :-
+  reply_json_dict(_{
+    success: true,
+    message: Message,
+    data: Data
+  }).
+
+success_response(Message, Data, StatusCode) :-
+  reply_json_dict(_{
+    success: true,
+    message: Message,
+    data: Data
+  }, [status(StatusCode)]).
+
+error_response(Message, Data) :-
+  reply_json_dict(_{
+    success: false,
+    message: Message,
+    data: Data
+  }).
+
+error_response(Message, Data, StatusCode) :-
+  reply_json_dict(_{
+    success: false,
+    message: Message,
+    data: Data
+  }, [status(StatusCode)]).

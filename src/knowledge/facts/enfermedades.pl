ac8f54dbe212e7a37073bf0ac9faefd149e54d5c
miagv <vitaliagual16@gmail.com>
2026-06-02 18:07:51 -0500

    cambios sintomas

--- src/knowledge/facts/enfermedades.pl
+++ src/knowledge/facts/enfermedades.pl
@@ -4,3 +4,23 @@
 
 enfermedad(gripe).
 enfermedad(covid).
+enfermedad(acne).
+enfermedad(dermatitis_atopica).
+enfermedad(dermatitis_de_contacto).
+enfermedad(urticaria).
+enfermedad(psoriasis).
+enfermedad(varicela).
+enfermedad(herpes_labial).
+enfermedad(herpes_zoster).
+enfermedad(tina).
+enfermedad(impetigo).
+enfermedad(dengue).
+enfermedad(zika).
+enfermedad(chikungunya).
+enfermedad(mononucleosis_infecciosa).
+enfermedad(tuberculosis).
+enfermedad(hepatitis_a).
+enfermedad(hepatitis_b).
+enfermedad(sarampion).
+enfermedad(rubeola).
+enfermedad(paperas).
